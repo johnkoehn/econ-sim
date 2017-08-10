4e4cfa8cec75136b7a63c9a7cd32fc69b38d1549
John Koehn <kingkoehn@gmail.com>
2017-08-10 11:31:38 -0500

    Built out parts of simulation and village_mind

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -3,6 +3,7 @@ pub mod village_manager;
 use self::village_manager::*;
 use village::*;
 use village_mind::*;
+use village_mind::trade_request::*;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -30,6 +31,34 @@ impl Simulation {
 
     pub fn village_managers(&self) -> &Vec<VillageManager> {
         &self.village_managers
+    }
+
+    pub fn simulate(&mut self) {
+
+        //update the villages and village minds with the new information
+        for iter in self.village_managers.iter_mut() {
+            iter.village.borrow_mut().simulate();
+            iter.village_mind.manage_village();
+        }
+
+        //do the trading phase until no more trade request are given
+        let mut trading = true;
+        let mut trade_requests : Vec<TradeRequest> = Vec::new();
+        while trading {
+            trade_requests.clear();
+            for mut iter in self.village_managers.iter_mut() {
+                trade_requests.append(&mut iter.village_mind.trade());
+            }
+
+            if trade_requests.len() == 0 {
+                trading = false;
+            }
+        }
+
+        //village minds will re-update after trading phase
+        for iter in self.village_managers.iter_mut() {
+            iter.village_mind.manage_village();
+        }
     }
 }
 
--- src/village_mind/mod.rs
+++ src/village_mind/mod.rs
@@ -1,4 +1,7 @@
+pub mod trade_request;
+
 use village::*;
+use village_mind::trade_request::*;
 
 pub struct VillageMind {
     village: VillageRef,
@@ -9,5 +12,38 @@ impl VillageMind {
         VillageMind {
             village: village,
         }
+    }
+
+    //The village mind decides what it wants to buy and sell
+    //The function returns a vector of trade requests
+    pub fn trade(&mut self) -> Vec<TradeRequest> {
+        vec!()
+    }
+
+    //The village mind in will manage the village in this function call
+    //This involves prioritizing resources, reassigning workers, general village management
+    //and deciding what resources it wants to sell and buy
+    pub fn manage_village(&mut self) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use village::*;
+    use village::worker::*;
+    use village_mind::*;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    fn default_village_ref() -> Rc<RefCell<Village>> {
+        Rc::new(RefCell::new(Village::new(|w: &Worker| false)))
+    }
+
+    #[test]
+    fn trade_test() {
+        let v1 = default_village_ref();
+        let mut mind1 = VillageMind::new(v1);
+        assert_eq!(0, mind1.trade().len());
     }
 }
